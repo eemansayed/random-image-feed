4519db5292b2166c4a261704fd611963e2586bc0
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-31 10:44:56 +0000

    add function called getRandomNr

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement("img");
   img.src = `${unsplashURL}`;
 }
+
+function getRandomNr() {
+  return Math.floor(Math.random() * 10) + 300;
+}
